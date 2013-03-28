a3bb04d7e2de093455f0068857622788b928d50f
Rob Pilling <robpilling@gmail.com>
2013-03-28 21:32:14 +0000

    FreeBSD compilation fixes

--- src/sdb/os/freebsd/os.c
+++ src/sdb/os/freebsd/os.c
@@ -1,6 +1,28 @@
+#include <strings.h>
 #include <errno.h>
 
+#include <sys/user.h>
+#include <sys/ptrace.h>
+
 #include "../../arch.h"
+
+int arch_reg_offset(const char *s)
+{
+//#define REG(x) if(!strcasecmp(s, #x)) return x;
+//#include "regs.def"
+//#undef REG
+	return 0;
+	return -1;
+}
+
+int arch_pseudo_reg(enum pseudo_reg r)
+{
+	switch(r){
+		case ARCH_REG_IP: return 0; //RIP;
+		case ARCH_REG_SP: return 1; //RSP;
+	}
+	return -1;
+}
 
 int arch_read(pid_t pid, addr_t addr, void *p, size_t l)
 {
@@ -14,13 +36,13 @@ int arch_write(pid_t pid, addr_t addr, const void *p, size_t l)
 	return -1;
 }
 
-int arch_reg_read(pid_t pid, enum arch_reg r, reg_t *p)
+int arch_reg_read(pid_t pid, int off, reg_t *p)
 {
 	errno = ENOSYS;
 	return -1;
 }
 
-int arch_reg_write(pid_t pid, enum arch_reg r, const reg_t v)
+int arch_reg_write(pid_t pid, int off, const reg_t v)
 {
 	// TODO
 	errno = ENOSYS;
