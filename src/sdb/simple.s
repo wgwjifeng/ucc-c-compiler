6767ab01eba5d00dcaeea466ff021170c937e37a
Rob Pilling <robpilling@gmail.com>
2013-03-31 21:16:33 +0100

    Added loop to asm-test file

--- src/sdb/simple.s
+++ src/sdb/simple.s
@@ -1,6 +1,30 @@
 .globl _start
 _start:
-	movl $60, %eax
+	movl $0, %ebx
+
+for:
+	call print
+	incl %ebx
+	cmp $10, %ebx
+	jne for
+
 	movl $5, %edi
+	call exit
+
+exit:
+	movl $60, %eax
 	syscall
 	hlt
+
+print:
+	pushq %rbx
+	subq $8, %rsp
+	movl $0x0a6968, (%rsp)
+	movl $1, %edi
+	movq %rsp, %rsi
+	movl $3, %edx
+	movl $1, %eax
+	syscall
+	addq $8, %rsp
+	popq %rbx
+	ret
