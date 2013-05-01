30a66fbbeca62a933995cbcf67bfeb202021a1fd
Rob Pilling <robpilling@gmail.com>
2013-05-01 21:33:59 +0100

    Added SDB_{AT,DE}TACH to FreeBSD os.c

--- src/sdb/os/freebsd/ptrace.c
+++ src/sdb/os/freebsd/ptrace.c
@@ -10,6 +10,8 @@ long os_ptrace(enum os_pt req, pid_t pid, void *addr, void *data)
 		case SDB_TRACEME: r = PT_TRACE_ME; break;
 		case SDB_SINGLESTEP: r = PT_STEP; break;
 		case SDB_CONT: r = PT_CONTINUE; break;
+		case SDB_ATTACH: r = PT_ATTACH; break;
+		case SDB_DETACH: r = PT_DETACH; break;
 	}
 
 	return ptrace(r, pid, addr, (intptr_t)data);
