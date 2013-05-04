bfa46de3f639e287713027e2df61cfe626efa0ab
Rob Pilling <robpilling@gmail.com>
2013-05-04 12:33:02 +0100

    Darwin mem read/write arch backend

--- src/sdb/os/darwin/os.c
+++ src/sdb/os/darwin/os.c
@@ -7,6 +7,7 @@
 #include <sys/ptrace.h>
 
 #include <mach/mach.h>
+#include <mach/mach_vm.h>
 
 #include "../../arch.h"
 #include "../../util.h" /* warn() */
@@ -74,18 +75,81 @@ void arch_detach(struct arch_proc **pap)
 	*pap = NULL;
 }
 
+static int darwin_set_errno(kern_return_t kr)
+{
+	switch(kr){
+		case KERN_SUCCESS:
+			errno = 0;
+			return 0;
+
+		case KERN_PROTECTION_FAILURE:
+		case KERN_INVALID_ADDRESS:
+			errno = EFAULT;
+			break;
+		case KERN_NO_SPACE:
+			errno = ENOMEM;
+			break;
+		case KERN_INVALID_ARGUMENT:
+		default:
+			errno = EINVAL;
+	}
+	return -1;
+}
+
 int arch_mem_read(const struct arch_proc *ap, addr_t addr, word_t *p)
 {
-	/* TODO: vm_read() */
-	errno = ENOSYS;
-	return -1;
+	struct arch_proc_darwin *dap = (struct arch_proc_darwin *)ap;
+
+	unsigned long n = 1; /* one word please */
+
+	/* why vm_read* takes a long instead of a pointer I'll never know.. */
+	kern_return_t kr = vm_read_overwrite(
+			dap->port, addr, sizeof *p, (word_t)p, &n);
+
+	return darwin_set_errno(kr);
 }
 
 int arch_mem_write(const struct arch_proc *ap, addr_t addr, word_t l)
 {
-	/* TODO: vm_write() */
-	errno = ENOSYS;
-	return -1;
+	/* typedefs... typedefs everywhere...
+	 * still less portable than using longs.
+	 * vm_* for 32-bit and mach_vm_* for 64. ILP64?
+	 */
+	struct arch_proc_darwin *dap = (struct arch_proc_darwin *)ap;
+
+	/* read a page... to change a single word... */
+	vm_size_t page_sz = 0x1000;
+	host_page_size(mach_host_self(), &page_sz);
+
+	mach_vm_address_t addr_page = addr & ~(page_sz - 1);
+	mach_vm_address_t off = addr - addr_page;
+	mach_msg_type_number_t whole_page_sz = page_sz;
+	pointer_t whole_page;
+
+	/* allocate a page... */
+	kern_return_t kr = mach_vm_read(
+			dap->port, addr_page, page_sz,
+			&whole_page, &whole_page_sz);
+
+	if(kr != KERN_SUCCESS)
+		goto bad;
+
+	/* change the single word... */
+	*(word_t *)((char *)whole_page + off) = l;
+
+	/* just let us */
+	mach_vm_protect(
+			dap->port, addr_page, page_sz,
+			0, VM_PROT_READ | VM_PROT_WRITE | VM_PROT_EXECUTE);
+
+	/* write the whole page back... */
+	kr = mach_vm_write(dap->port, addr_page, whole_page, whole_page_sz);
+
+	/* and deallocate our copy of the page */
+	mach_vm_deallocate(mach_task_self(), whole_page, whole_page_sz);
+
+bad:
+	return darwin_set_errno(kr);
 }
 
 int arch_reg_read(const struct arch_proc *ap, int off, reg_t *p)
