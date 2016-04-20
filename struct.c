bd4de8e3c47567537b4004700b4929838a7c8df4
Rob Pilling <robpilling@gmail.com>
2016-04-20 20:50:05 +0100

    TODO: test case for -fdump-struct-layout

---
+++ struct.c
@@ -0,0 +1,17 @@
+struct A
+{
+	struct B
+	{
+		int j;
+		char c;
+	} b;
+	char x;
+} a;
+
+struct data
+{
+	unsigned int foo:4;
+	unsigned int bar:4;
+	unsigned char other;
+	unsigned short other2;
+} c;
