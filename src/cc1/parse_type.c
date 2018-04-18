47668986204e85df54ecc091ca276f9fa257f5c0
Rob Pilling <robpilling@gmail.com>
2018-04-18 21:04:08 +0100

    Remove superflous brackets around cc1_fopt checks

--- src/cc1/parse_type.c
+++ src/cc1/parse_type.c
@@ -1264,7 +1264,7 @@ static type_parsed *parsed_type_array(
 					 * (and we don't have the fold-const-vlas setting), then treat
 					 * as a vla */
 					if(k.type != CONST_NUM
-					|| (k.nonstandard_const && !(cc1_fopt.fold_const_vlas)))
+					|| (k.nonstandard_const && !cc1_fopt.fold_const_vlas))
 					{
 						is_vla = VLA;
 					}
